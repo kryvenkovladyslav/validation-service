c34e7a20410fd9d7c3e69305198f8e2924d73d64
DESKTOP-38Q3HR9\Vladyslav Kryvenko <onekryvenko.vladyslav@gmail.com>
2024-03-14 20:55:17 +0300

    Created a Domain layer of the system

---
+++ src/Domain/Abstract/IDocumentValidationStrategy.cs
@@ -0,0 +1,22 @@
+﻿using System.IO;
+using Domain.Models;
+using System.Threading.Tasks;
+
+namespace Domain.Abstract
+{
+    public interface IDocumentValidationStrategy
+    {
+        /// <summary>
+        /// Provides a boolean result if the file can be processed 
+        /// </summary>
+        /// <param name="documentStream">A required document to be processed</param>
+        /// <returns>True if a file can be processed</returns>
+        public bool CanProcess(Stream documentStream);
+
+        /// <summary>
+        /// Asynchronously processes the file
+        /// </summary>
+        /// <returns>Provided validation result</returns>
+        public Task<ValidationResult> ProcessAsync(RequestModel requestModel);
+    }
+}
---
+++ src/Domain/Models/RequestModel.cs
@@ -0,0 +1,29 @@
+﻿using System.IO;
+
+namespace Domain.Models
+{
+    /// <summary>
+    /// The model represent a request to File Storage
+    /// </summary>
+    public sealed class RequestModel
+    {
+        /// <summary>
+        /// The stream representing a current document
+        /// </summary>
+        public Stream DocumentStream { get; init; }
+
+        /// <summary>
+        /// The full path to a required file
+        /// </summary>
+        public string RequiredFilePath { get; init; }
+
+        /// <summary>
+        /// The constructor for initialization the instance
+        /// </summary>
+        public RequestModel(string requiredFilePath, Stream documentStream) 
+        {
+            this.DocumentStream = documentStream;
+            this.RequiredFilePath = requiredFilePath;
+        }
+    }
+}
